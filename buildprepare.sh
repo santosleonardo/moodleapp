880871c6420d77763db33d4d76e1b320f57f726a
Leonardo Santos <leonardo.santos@unirio.br>
2017-03-20 15:51:02 -0300

    Adaptações no script de preparação para build.

--- buildprepare.sh
+++ buildprepare.sh
@@ -1,40 +1,42 @@
 #!/bin/bash
 
-# Criar fork do repositorio https://github.com/moodlehq/moodlemobile-phonegapbuild.git e clonar
+# Quando ha atualizacoes em https://github.com/moodlehq/moodlemobile-phonegapbuild.git e necessario remover o fork criado e cria-lo novamente com as atualizacoes.
 
 cd ..
 
-git clone https://github.com/santosleonardo/moodlemobile-phonegapbuild.git
+if [ ! -d  moodlemobile-phonegapbuild ]; then
+    git clone https://github.com/santosleonardo/moodlemobile-phonegapbuild.git
+fi
 
 cd moodlemobile-phonegapbuild
 
-if [ $(git branch -a | grep avpmobile | wc -l) -gt 0 ]
+if [ $(git branch -a | grep posalfa | wc -l) -gt 0 ]
 then
-    git checkout avpmobile
+    git checkout posalfa
 else
-    git checkout -b avpmobile
+    git checkout -b posalfa master
+    
+    sed -e 's/com.moodle.moodlemobile/br.pro.avp.posalfa/' -i .OLD config.xml
+    sed -e 's/>Moodle Mobile/>POSALFA/' -i .OLD config.xml
+    sed -e 's/Moodle Mobile official app/PîS-GRADUA‚ÌO LATO SENSU ALFABETIZA‚ÌO DAS CRIAN‚AS DAS CLASSES POPULARES/' -i .OLD config.xml
+    sed -e 's/http:\/\/moodle.com/http:\/\/www.avp.pro.br/' -i .OLD config.xml
+    sed -e 's/mobile@moodle.com/posalfauff@gmail.com/' -i .OLD config.xml
+    sed -e 's/Moodle Pty Ltd/Carmen Peres/' -i .OLD config.xml
+    sed -e 's/value=\"moodlemobile\"/value=\"posalfa\"/' -i .OLD config.xml
+    sed -e 's/debuggable=\"true\"/debuggable=\"false\"/' -i .OLD config.xml
+    rm config.xml.OLD
+    
+    sed -e 's/true)/false)/' -i .OLD core/lib/log.js
+    rm core/lib/log.js.OLD
 fi
 
 cp -R ../moodlemobile2/www/* .
 
 cp -R ../moodlemobile2/resources/* resources/
 
-cp ../moodlemobile2/resources/icon.png ../moodlemobile2/resources/splash.png .
-
-sed -e 's/com.moodle.moodlemobile/br.pro.avp.avpmobile/' -i .OLD config.xml
-sed -e 's/>Moodle Mobile/>AVP Mobile/' -i .OLD config.xml
-sed -e 's/Moodle Mobile official app/Aplicativo Oficial do Ambiente Virtual de Pesquisa/' -i .OLD config.xml
-sed -e 's/http:\/\/moodle.com/http:\/\/www.avp.pro.br/' -i .OLD config.xml
-sed -e 's/mobile@moodle.com/mauriciorochacruz@gmail.com/' -i .OLD config.xml
-sed -e 's/Moodle Pty Ltd/Mauricio Cruz/' -i .OLD config.xml
-sed -e 's/value=\"moodlemobile\"/value=\"avpmobile\"/' -i .OLD config.xml
-sed -e 's/debuggable=\"true\"/debuggable=\"false\"/' -i .OLD config.xml
-rm config.xml.OLD
-
-sed -e 's/true)/false)/' -i .OLD core/lib/log.js
-rm core/lib/log.js.OLD
+cp ../moodlemobile2/resources/splash.png ../moodlemobile2/resources/android/icon.png .
 
 git add .
 git commit -m "Version update."
-git push origin avpmobile
+git push origin posalfa
 
