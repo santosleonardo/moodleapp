8d4d239d0e4170e024c7411ff90ac40ed911e662
Leonardo Santos <leonardo.santos@unirio.br>
2017-03-21 15:53:29 -0300

    Fundo transparente nos ícones android, traduções no módulo de tarefas, alteração na tela de login.

--- image_round_corners.sh
+++ image_round_corners.sh
@@ -15,9 +15,16 @@ function image_round_corners() {
      \) -alpha off -compose CopyOpacity -composite  $3/$2
 }
 
-image_round_corners drawable-ldpi-icon.png icon-36-ldpi.png resources/android/icon
-image_round_corners drawable-mdpi-icon.png icon-48-mdpi.png resources/android/icon
-image_round_corners drawable-hdpi-icon.png icon-72-hdpi.png resources/android/icon
-image_round_corners drawable-xhdpi-icon.png icon-96-xhdpi.png resources/android/icon
-image_round_corners drawable-xxhdpi-icon.png icon-144-xxhdpi.png resources/android/icon
-image_round_corners drawable-xxxhdpi-icon.png icon-192-xxxhdpi.png resources/android/icon
+#image_round_corners drawable-ldpi-icon.png icon-36-ldpi.png resources/android/icon
+#image_round_corners drawable-mdpi-icon.png icon-48-mdpi.png resources/android/icon
+#image_round_corners drawable-hdpi-icon.png icon-72-hdpi.png resources/android/icon
+#image_round_corners drawable-xhdpi-icon.png icon-96-xhdpi.png resources/android/icon
+#image_round_corners drawable-xxhdpi-icon.png icon-144-xxhdpi.png resources/android/icon
+#image_round_corners drawable-xxxhdpi-icon.png icon-192-xxxhdpi.png resources/android/icon
+
+cp resources/android/icon/drawable-ldpi-icon.png resources/android/icon/icon-36-ldpi.png
+cp resources/android/icon/drawable-mdpi-icon.png resources/android/icon/icon-48-mdpi.png 
+cp resources/android/icon/drawable-hdpi-icon.png resources/android/icon/icon-72-hdpi.png
+cp resources/android/icon/drawable-xhdpi-icon.png resources/android/icon/icon-96-xhdpi.png
+cp resources/android/icon/drawable-xxhdpi-icon.png resources/android/icon/icon-144-xxhdpi.png
+cp resources/android/icon/drawable-xxxhdpi-icon.png resources/android/icon/icon-192-xxxhdpi.png
